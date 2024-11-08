07d3c5c973e963136afb8dc32760c7d7de062bd3
Kwanghoon Son <kwangson@yahoo.com>
2024-11-08 17:11:26 +0900

    Fix wrong url
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -9,10 +9,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
